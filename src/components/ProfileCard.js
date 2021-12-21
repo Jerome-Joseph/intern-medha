d77e1ce5fb528b40ecd95c216b49dec33ab000ba
Jerome-Joseph <jeromejoseph782@gmail.com>
2021-12-21 23:05:38 +0530

    commit 2

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
  const ProfileCard = (props) => { 
 
@@ -18,6 +19,7 @@ import { useEffect, useState } from 'react';
          console.log(data[0].jobseeker_name)
          console.log(articles)
     }
+    
 
     useEffect(() => {
         fetchData();
